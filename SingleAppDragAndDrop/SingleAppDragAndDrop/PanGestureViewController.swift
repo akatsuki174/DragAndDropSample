032389d1b6bfb1ab49745ede8950504a76de11da
akatsuki174 <dev2015js@gmail.com>
2018-01-20 21:54:04 +0900

    PanGestureを追加

--- SingleAppDragAndDrop/SingleAppDragAndDrop/PanGestureViewController.swift
+++ SingleAppDragAndDrop/SingleAppDragAndDrop/PanGestureViewController.swift
@@ -6,7 +6,6 @@ class PanGestureViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
 
     override func didReceiveMemoryWarning() {
@@ -14,5 +13,17 @@ class PanGestureViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-}
+    private func transform(for translation: CGPoint) -> CGAffineTransform {
+        let moveBy = CGAffineTransform(translationX: translation.x, y:  translation.y)
+        let rotation = -sin(translation.x / (imageView.frame.width * 4.0))
+        return moveBy.rotated(by: rotation)
+    }
 
+    @IBAction func panAction(_ sender: UIPanGestureRecognizer) {
+        let move: CGPoint = sender.translation(in: view)
+        sender.view!.center.x += move.x
+        sender.view!.center.y += move.y
+        sender.setTranslation(CGPoint.zero, in:view)
+    }
+
+}
