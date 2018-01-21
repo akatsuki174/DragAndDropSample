789fc325a7202b36c89af0bb5bf3b5a0daf5a7c8
akatsuki174 <dev2015js@gmail.com>
2018-01-21 09:54:24 +0900

    ページを1つ追加

--- SingleAppDragAndDrop/SingleAppDragAndDrop/PanGestureViewController.swift
+++ SingleAppDragAndDrop/SingleAppDragAndDrop/PanGestureViewController.swift
@@ -10,7 +10,6 @@ class PanGestureViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     private func transform(for translation: CGPoint) -> CGAffineTransform {
