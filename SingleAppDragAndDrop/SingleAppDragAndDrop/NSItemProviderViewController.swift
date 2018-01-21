f28629ca7a86ffe9b019590fd6ed86dce75ca7f9
akatsuki174 <dev2015js@gmail.com>
2018-01-21 12:26:15 +0900

    dragするのに最低限必要な実装を追加

--- SingleAppDragAndDrop/SingleAppDragAndDrop/NSItemProviderViewController.swift
+++ SingleAppDragAndDrop/SingleAppDragAndDrop/NSItemProviderViewController.swift
@@ -2,8 +2,13 @@ import UIKit
 
 class NSItemProviderViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let dragInteraction = UIDragInteraction(delegate: self)
+        imageView.addInteraction(dragInteraction)
     }
 
     override func didReceiveMemoryWarning() {
@@ -11,3 +16,18 @@ class NSItemProviderViewController: UIViewController {
     }
     
 }
+extension NSItemProviderViewController: UIDragInteractionDelegate {
+
+    func dragInteraction(_ interaction: UIDragInteraction, itemsForBeginning session: UIDragSession) -> [UIDragItem] {
+        guard let image = imageView.image else {
+            return []
+        }
+
+        let itemProvider = NSItemProvider(object: image)
+        let item = UIDragItem(itemProvider: itemProvider)
+        item.localObject = image
+        return [item]
+    }
+
+}
+
